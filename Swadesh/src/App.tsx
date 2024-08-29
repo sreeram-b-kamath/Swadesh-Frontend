547a0234b2ac4603310e01108e5979a1ba8b39ab
denamaria <dena01george@gmail.com>
2024-08-29 11:52:36 +0530

    setting up routing in chef page

--- Swadesh/src/App.tsx
+++ Swadesh/src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-// import RoutesComponent from './routes/Route';
+// import AddToMenu from "./pages/AddToMenu";
+import RoutesComponent from './routes/Route';
 // import LoginPage from "./pages/LoginPage/LoginPage";
-import CustomCategories from "./pages/CustomCategories";
-import CustomRestrictions from "./pages/CustomRestrictions";
+// import CustomCategories from "./pages/CustomCategories";
+// import CustomRestrictions from "./pages/CustomRestrictions";
 
 const App = () => {
   return (
     <div>
-      {/* <RoutesComponent /> */}
-      <CustomCategories/>
-      <CustomRestrictions/>    </div>
+      <RoutesComponent />
+      {/* <CustomCategories/>
+      <CustomRestrictions/>    */}
+
+       </div>
   );
 };
 
--- Swadesh/src/routes/Route.tsx
+++ Swadesh/src/routes/Route.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from '../components/LoginSignUp/SignUp';
 import OtpVerificationPage from '../components/LoginSignUp/OTP';
 import LoginPage from '../pages/LoginPage/LoginPage';
+import CustomRestrictions from '../pages/CustomRestrictions';
+import CustomCategories from '../pages/CustomCategories';
+import AddToMenu from '../pages/AddToMenu';
 
 const RoutesComponent = () => {
   return (
@@ -13,6 +16,9 @@ const RoutesComponent = () => {
         <Route path="/otp-verification" element={<OtpVerificationPage onSubmit={function (otp: string): void {
                   throw new Error('Function not implemented.');
               } } />} />
+         <Route path="/add-to-menu" element={<AddToMenu />} />
+         <Route path="/categories" element={<CustomCategories />} />
+         <Route path="/restrictions" element={<CustomRestrictions />} />
       </Routes>
     </BrowserRouter>
   );
