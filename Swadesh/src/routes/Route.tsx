021895224f90b972410e99c7a5853f5a317fdb22
Thulasi k <thulasik112001@gmail.com>
2024-08-30 13:05:56 +0530

    safe copy commit

--- Swadesh/src/routes/Route.tsx
+++ Swadesh/src/routes/Route.tsx
@@ -1,27 +1,18 @@
 // routes.tsx
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignUp from "../components/LoginSignUp/SignUp";
-import OtpVerificationPage from "../components/LoginSignUp/OTP";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import AddToMenu from "../pages/AddToMenu";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from '../components/LoginSignUp/SignUp';
+import OtpVerificationPage from '../components/LoginSignUp/OTP';
+import LoginPage from '../pages/LoginPage/LoginPage';
 
 const RoutesComponent = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path = "/" element = {<LoginPage />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route
-          path="/otp-verification"
-          element={
-            <OtpVerificationPage
-              onSubmit={function (otp: string): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-          }
-        />
-        <Route path="/AddToMenu" element={<AddToMenu />}></Route>
+        <Route path="/otp-verification" element={<OtpVerificationPage onSubmit={function (otp: string): void {
+                  throw new Error('Function not implemented.');
+              } } />} />
       </Routes>
     </BrowserRouter>
   );
